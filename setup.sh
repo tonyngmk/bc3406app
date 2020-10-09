6c81c5edf61a82896d34c85ea25e2d03ed64d487
Tony Ng <TONY0006@e.ntu.edu.sg>
2020-10-09 09:50:20 +0800

    Changed setup file

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"TONY0006@e.ntu.edu.sg\"\n\
+email = \"tony00006@e.ntu.edu.sg\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
